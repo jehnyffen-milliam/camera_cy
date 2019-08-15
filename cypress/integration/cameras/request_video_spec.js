5cc0675bdf6071d6e55bc30efa6d9ec01efedff1
Jehny <jehnyffen@gmail.com>
2019-08-15 06:43:28 -0300

    taking comments of request video

--- cypress/integration/cameras/request_video_spec.js
+++ cypress/integration/cameras/request_video_spec.js
@@ -9,25 +9,8 @@ describe('Validate Request Video', function(){
 		cy.menuCamera();
 	})
 
-	// it('List event Video', function(){
-	// 	cy.clickAllVehicleGoups()
-	// 	cy.clickStartDate('23/07/2019')
-	// 	cy.clickEndDate('24/07/2019')
-
-	// 	cy.clickButton('Apply Filter');
-	// 	cy.get('*').contains('[VINCENTFMT]')
-	// 	//Click request Video
-	// 	cy.clickRequestVideoByVehicle2('[VINCENTFMT]', '2019-07-23 17:57:38');
-	// 	cy.get('*').contains('New Message')
-	// 	cy.get('*').contains('Video requested. You will be notified when it is ready to download');
-	// 	cy.screenshot('message-vide-requested', 'fullPage')
-	// 	// can be verified if button is disabled
-	// })
-
 	it('Request Video Manualy', function(){
-		// cy.visit('http://move.transpoco.net')
-		// cy.login('jehnyffen.milliam@transpoco.com', '0128@Milliamj')
-		// cy.menuCamera();
+		
 		cy.wait(4000)
 		cy.clickAllVehicleGoups()
 		cy.clickStartDate('13/08/2019')
@@ -47,20 +30,19 @@ describe('Validate Request Video', function(){
 		cy.get('*').contains('Select a Date').click();
 		cy.wait(1000)
 		cy.setTimeRequestVideo('02:05:16')
-		// cy.wrap({changeTime: setTime}).invoke('changeTime')
 		cy.get('#confirm_button').contains('Request Video').click()
-		// cy.get('*').contains('New Message')
-		// cy.get('*').contains('Video requested. You will be notified when it is ready to download');
-		// cy.wait(4000)
-		// cy.get('.fa-refresh').click()
-		// cy.wait(4000)
-		// cy.get('*').contains('Manual request')
-		// cy.wait(2000)
+		cy.get('*').contains('New Message')
+		cy.get('*').contains('Video requested. You will be notified when it is ready to download');
+		cy.wait(4000)
+		cy.get('.fa-refresh').click()
+		cy.wait(4000)
+		cy.get('*').contains('Manual request')
+		cy.wait(2000)
 	})
 
-	// after(function() {
-	//     // runs once after all tests in the block
-	//     cy.logOut('Log out')
-	//  })
+	after(function() {
+	    // runs once after all tests in the block
+	    cy.logOut('Log out')
+	 })
 
 })
